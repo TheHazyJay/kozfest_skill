ba5893170d503ebf7d827608a51d3579d3fc0b75
TheHazyJay <48600216+TheHazyJay@users.noreply.github.com>
2019-03-15 17:17:14 +0000

    Create location.js

---
+++ location.js
@@ -0,0 +1,12 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent'] */
+// TODO: Replace this data with your own.
+module.exports = {
+  'RECIPE_EN_GB': {
+    'postcode': 'Ask me again in a day or two',
+    'directions': 'I can give you directions soon, I\'m waiting to be given the exact route',
+    'location': 'A beautiful site in North Devon, near woollacombe, overlooking the sea. This year, there is no hill to climb either, we have a much flatter site for your convenience',
+    'address': 'That information is coming soon',
+  },
+};
